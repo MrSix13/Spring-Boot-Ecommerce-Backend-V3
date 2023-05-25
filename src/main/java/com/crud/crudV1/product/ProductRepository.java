cb63f9d13631d8ec4e97cb14f19bb463c04f1431
bastian <bastiangarcesga@gmail.com>
2023-05-25 18:59:36 -0400

    ADD: Product Repository

--- src/main/java/com/crud/crudV1/product/ProductRepository.java
+++ src/main/java/com/crud/crudV1/product/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.crud.crudV1.product;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/crud/crudV1/product/ProductService.java
+++ src/main/java/com/crud/crudV1/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.crud.crudV1.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,13 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
     public List<Product> getProducts(){
-        return List.of(
-                new Product(
-                        21312L,
-                        "Laptop MAC",
-                        450,
-                        LocalDate.of(2025, Month.MARCH, 5),
-                        2
-                )
-        );
+        return this.productRepository.findAll();
     }
 }
